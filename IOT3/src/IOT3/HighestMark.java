3abe6a2c0532c7ff6c36b4dd76172c70a5d25c8d
vijayaraghavendrad-hub <vijayaraghavendrad@gmail.com>
2026-08-20 16:26:29 +0530

    First commit

---
+++ IOT3/src/IOT3/HighestMark.java
@@ -0,0 +1,19 @@
+package IOT3;
+
+public class HighestMark {
+
+    public static void main(String[] args) {
+
+        int[] marks = {78, 65, 89, 92, 71};
+
+        int highest = marks[0];
+
+        for (int i = 1; i < marks.length; i++) {
+
+            if (marks[i] > highest) {
+                highest = marks[i];
+            }
+        }
+        System.out.println("Highest Marks = " + highest);
+    }
+}
---
+++ IOT3/src/IOT3/PassedStudents.java
@@ -0,0 +1,20 @@
+package IOT3;
+
+public class PassedStudents {
+
+    public static void main(String[] args) {
+
+        int[] marks = {35, 67, 89, 22, 45, 30, 76};
+
+        int count = 0;
+
+        for (int i = 0; i < marks.length; i++) {
+
+            if (marks[i] >= 40) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of passed students = " + count);
+    }
+}
